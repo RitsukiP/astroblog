acd8b8c715d5f370eb58890d88e3ae976e043ac5
RitsukiP <me.simon_R@outlook.com>
2026-07-04 14:19:32 +1000

    add html files

--- src/config.ts
+++ src/config.ts
@@ -3,39 +3,40 @@ import type {
   NavBarConfig,
   ProfileConfig,
   SiteConfig,
-} from './types/config'
-import { LinkPreset } from './types/config'
+} from "./types/config";
+import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: 'ObservatoryX',
-  subtitle: '天文觀測站',
-  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  title: "ObservatoryX",
+  subtitle: "天文觀測站",
+  lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
-    hue: 180,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: true,     // Hide the theme color picker for visitors
+    hue: 180, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: true, // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
-    src: 'assets/images/bg.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    src: "assets/images/bg.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
-    }
+      enable: false, // Display the credit text of the banner image
+      text: "", // Credit text to be displayed
+      url: "", // (Optional) URL link to the original artwork or artist's page
+    },
   },
   toc: {
-    enable: true,           // Display the table of contents on the right side of the post
-    depth: 2                // Maximum heading depth to show in the table, from 1 to 3
+    enable: true, // Display the table of contents on the right side of the post
+    depth: 2, // Maximum heading depth to show in the table, from 1 to 3
   },
-  favicon: [    // Leave this array empty to use the default favicon
+  favicon: [
+    // Leave this array empty to use the default favicon
     // {
     //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
     //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     // }
-  ]
-}
+  ],
+};
 
 export const navBarConfig: NavBarConfig = {
   links: [
@@ -44,43 +45,43 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Friends,
     LinkPreset.About,
     {
-      name: '旧站（已弃用）',
-      url: 'https://ObservatoryX.github.io',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
+      name: "English (Tech related)",
+      url: "/english/en_index.html", // Internal links should not include the base path, as it is automatically added
+      external: true, // Show an external link icon and will open in a new tab
     },
   ],
-}
+};
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/IMG_7218.JPG',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Ethan Chen',
-  bio: 'El pueblo unido jamás será vencido.',
+  avatar: "assets/images/IMG_7218.JPG", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: "Ethan Chen",
+  bio: "El pueblo unido jamás será vencido.",
   links: [
     {
-      name: 'Telegram',
-      icon: 'fa6-brands:telegram',
-      url: 'https://t.me/Leviticus_Aru',
+      name: "Telegram",
+      icon: "fa6-brands:telegram",
+      url: "https://t.me/Leviticus_Aru",
     },
     {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/RitsukiP',
+      name: "GitHub",
+      icon: "fa6-brands:github",
+      url: "https://github.com/RitsukiP",
     },
     {
-      name: 'Bluesky',
-      icon: 'fa6-brands:bluesky',
-      url: 'https://bsky.app/profile/leviticus-aru.bsky.social',
+      name: "Bluesky",
+      icon: "fa6-brands:bluesky",
+      url: "https://bsky.app/profile/leviticus-aru.bsky.social",
     },
     {
-      name: 'RSS',
-      icon: 'fa6-solid:rss',
-      url: 'https://observatoryx.vercel.app/rss.xml',
+      name: "RSS",
+      icon: "fa6-solid:rss",
+      url: "https://observatoryx.vercel.app/rss.xml",
     },
   ],
-}
+};
 
 export const licenseConfig: LicenseConfig = {
   enable: true,
-  name: 'CC BY-NC-SA 4.0',
-  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
-}
+  name: "CC BY-NC-SA 4.0",
+  url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
